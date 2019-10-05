d074c970394a032019d6ddec00a8d5f8e473dc4d
Dan Choiniere <dchoinie@gmail.com>
2019-10-04 19:42:55 -0500

    Products created in snipcart

---
+++ src/components/products.js
@@ -0,0 +1,46 @@
+import React from "react"
+import Product from "./product"
+import { StaticQuery, graphql } from "gatsby"
+
+const getProducts = graphql`
+  {
+    products: allContentfulMembershipType {
+      edges {
+        node {
+          id
+          title
+          price
+          description
+          category
+          image {
+            fluid {
+              src
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default () => {
+  return (
+    <div>
+      <StaticQuery
+        query={getProducts}
+        render={data => {
+          return (
+            <section className="">
+              <div className="products-container">
+                {data.products.edges.map(({ node: product }) => {
+                  return <Product key={product.id} product={product} />
+                })}
+              </div>
+            </section>
+          )
+        }}
+      />
+    </div>
+  )
+}
--- src/pages/membership.js
+++ src/pages/membership.js
@@ -1,40 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
-
-export const membershipQuery = graphql`
-  query MyQuery {
-    allContentfulMembershipType {
-      nodes {
-        category
-        description
-        id
-        price
-        title
-        contentful_id
-        image {
-          fixed(width: 50, height: 50) {
-            ...GatsbyContentfulFixed
-          }
-        }
-      }
-    }
-  }
-`
+import Products from "../components/products"
 
 export default ({ data }) => {
   return (
     <Layout>
-      {/* {data.allContentfulMembershipType.nodes.map(node => (
-        <div key={node.contentful_id}>
-          <p>{node.title}</p>
-          <p>${node.price}</p>
-          <p>{node.description}</p>
-          <p>{node.category}</p>
-          <hr />
-        </div>
-      ))} */}
-      Membership
+      <Products />
     </Layout>
   )
 }
