daabc62e88a0559db06aaa65e5bb02e485654889
Dan Choiniere <dchoinie@gmail.com>
2019-10-09 10:25:14 -0500

    Yearly Memberhips Created
    Added snipcart to paypal so all payments will be sent to paypal account

--- src/pages/membership.js
+++ src/pages/membership.js
@@ -2,13 +2,15 @@ import React from "react"
 import Layout from "../components/layout"
 import DayPasses from "../components/dayPasses"
 import MonthlyMemberships from "../components/montlyMemberships"
+import YearlyMemberships from "../components/yearlyMemberships"
 
 export default () => {
   return (
     <Layout>
-      <div className="px-6 lg:px-0">
+      <div className="px-6 lg:px-0 my-12">
         <DayPasses />
         <MonthlyMemberships />
+        <YearlyMemberships />
       </div>
     </Layout>
   )
